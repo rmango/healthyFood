5dd24166e453a607c9179e346123b272c50da631
rmango <morgan-graham@live.com>
2019-03-24 00:30:12 -0700

    putting things on popup

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+
+
+
+console.log(recipes[0].title);
+document.getElementById("title").textContent = recipes[0].title;
+document.getElementById("img").setAttribute("src",recipes[2].img);
+document.getElementById("container").setAttribute("alt",recipes[2].link);
+
+
---
+++ js/recipes.js
@@ -0,0 +1,21 @@
+var recipes = [
+    {
+        "title": "salad",
+        "img": "salad.png",
+        "description": "blah blah",
+        "link": "https://google.com"
+    },
+    {
+        "title": "Chipotle Quinoa Chili",
+        "img": "img/QuinoaChili.jpg",
+        "description": "Quinoa Chipotle Chili Beans Grains Savory Dinner",
+        "link": "https://www.purplecarrot.com/plant-based-recipes/chipotle-quinoa-chili"
+    },
+    {
+        "title": "Potsticker soup",
+        "img": "img/potstickerSoup.jpg",
+        "description": "Potsticker soup soy sauce gyoza Savory Dinner",
+        "link": "https://www.traderjoes.com/recipes/dinner/potsticker-soups-on"
+    },
+]
+
