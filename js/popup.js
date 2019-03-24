a621a28ea1a46275f0427f665fa70377a385b8f1
rmango <morgan-graham@live.com>
2019-03-24 01:05:54 -0700

    link works

--- js/popup.js
+++ js/popup.js
@@ -7,4 +7,12 @@ document.getElementById("title").textContent = recipes[rand].title;
 document.getElementById("image").setAttribute("src",recipes[rand].img);
 document.getElementById("container").setAttribute("alt",recipes[rand].link);
 
+function openIndex() {
+    chrome.tabs.create({active: true, url: recipes[rand].link });
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+    var y = document.getElementById("container");
+    y.addEventListener("click", openIndex);
+
+ });
