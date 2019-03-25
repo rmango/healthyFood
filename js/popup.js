a7467cd527e3d37c7118b8d78cdfe667114ebf47
rmango <morgan-graham@live.com>
2019-03-25 01:24:34 -0700

    everything is still broken

--- js/popup.js
+++ js/popup.js
@@ -10,6 +10,46 @@ function openIndex() {
     chrome.tabs.create({ active: true, url: recipes[rand].link });
 }*/
 
+
+
+
+
+//functions
+function checkStatus(response) {
+    if (response.ok) {
+        return Promise.resolve(response);
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+function parseJSON(response) {
+    return response.json();
+}
+//for opening link in new tab
+function openIndex() {
+    chrome.tabs.create({ active: true, url: recipes[rand].link });
+}
+
+
+function promiseAllFunc(domString) {
+
+    //read in jsons from theMealDB
+    const urls = [
+        'https://www.themealdb.com/api/json/v1/1/list.php?i=list',
+        'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian'
+    ];
+
+    var valToReturn = Promise.all(urls.map(url =>
+        fetch(url)
+            .then(checkStatus)
+            .then(parseJSON)
+            .catch(error => console.log("problemo", error))
+    ))
+
+    return valToReturn;
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(tabs[0].id, { file: "js/content.js" }, function (data) {
@@ -21,19 +61,10 @@ document.addEventListener('DOMContentLoaded', function () {
             var ingredients;
             var vegRecipes;
             var ingredToInclude = [];
+            //array of meals that have matching ingredients
+            var mealMatches = [];
 
-            //read in all of the jsons from theMealDB
-            const urls = [
-                'https://www.themealdb.com/api/json/v1/1/list.php?i=list',
-                'https://www.themealdb.com/api/json/v1/1/filter.php?c=Vegetarian'
-            ];
-
-            Promise.all(urls.map(url =>
-                fetch(url)
-                    .then(checkStatus)
-                    .then(parseJSON)
-                    .catch(error => console.log("problemo", error))
-            ))
+            promiseAllFunc(domString) //reads in list of ingredients and list of vegetarian recipes
                 .then(allResponses => {
                     ingredients = allResponses[0];
                     vegRecipes = allResponses[1];
@@ -50,11 +81,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     //if there are ingredients on page
                     if (ingredToInclude.length > 0) {
-
-                        //array of meals that have matching ingredients
-                        var mealMatches = [];
-
                         console.log("ingredients were found!");
+                        
                         //compare recipe ingredients to ingredients found (if recipes exist). select recipe with highest num ingredients in common
                         for (var i = 0; i < vegRecipes.meals.length; i++) {
                             //console.log(vegRecipes.meals[i].strMeal);
@@ -76,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function () {
                                     //console.log("Ingredient: ", nextIngred);
                                     //if the recipe has an ingredient that was found on the domString
                                     if (ingredToInclude.toString().includes(nextIngred)) {
-                                        //console.log("MATCH for " + meal.meals[0].idMeal + ":", nextIngred);
+                                        console.log("MATCH for " + meal.meals[0].idMeal + ":", nextIngred);
                                         ingredMatches++;
                                     }
                                     ingredNum++;
@@ -87,6 +115,7 @@ document.addEventListener('DOMContentLoaded', function () {
                                     mealMatches.push({ "mealId": mealId, "mealName": meal.meals[0].strMeal, "mealImg": meal.meals[0].strMealThumb, "mealVid": meal.meals[0].strYoutube, "numMatches": 1 });
                                     console.log("pushed in " + i + ": ", mealMatches[mealMatches.length - 1]);
                                 }
+                                console.log("at end of request #", i);
                             }
                             //call to request, getting individual meal json
                             request();
@@ -94,14 +123,14 @@ document.addEventListener('DOMContentLoaded', function () {
                         }
                         //get the meal (with matching ingredients) with the most matches
                         console.log("test", mealMatches);
-                        console.log("test", mealMatches.length);
-                        console.log("test", mealMatches[0].mealId);
+                        //console.log("test", mealMatches.length);
+                        //console.log("test", mealMatches[0].mealId);
 
-                        document.getElementById("title").textContent = mealMatches[0].mealName;
+                        //document.getElementById("title").textContent = mealMatches[0].mealName;
 
                         /*if (mealMatches.length > 0) {
                             console.log("meal matches: ", mealMatches[0].mealId);
-
+            
                             var theOne = mealMatches[0];
                             /*var mostMatches = 0;
                             for (var k = 0; k < mealMatches.length; k++) {
@@ -113,7 +142,7 @@ document.addEventListener('DOMContentLoaded', function () {
                             //put null check on chosen
                             //set title
                             document.getElementById("title").textContent = theOne.mealName;
-
+            
                         } else {
                             //go random
                             console.log("randomize itttt");
@@ -126,14 +155,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
                 })
-                
-                //if all fails, go random
-                //if can't find good match, just go random
-                /*var rand = Math.floor((Math.random() * recipes.length));
+            console.log("work pls", mealMatches[0]);
 
-                document.getElementById("title").textContent = recipes[rand].title;
-                document.getElementById("image").setAttribute("src", recipes[rand].img);
-                document.getElementById("container").setAttribute("alt", recipes[rand].link);*/
+
+            //if all fails, go random
+            //if can't find good match, just go random
+            /*var rand = Math.floor((Math.random() * recipes.length));
+
+            document.getElementById("title").textContent = recipes[rand].title;
+            document.getElementById("image").setAttribute("src", recipes[rand].img);
+            document.getElementById("container").setAttribute("alt", recipes[rand].link);*/
 
 
 
@@ -142,21 +173,3 @@ document.addEventListener('DOMContentLoaded', function () {
     //var y = document.getElementById("container");
     //y.addEventListener("click", openIndex);
 });
-
-
-
-//functions
-function checkStatus(response) {
-    if (response.ok) {
-        return Promise.resolve(response);
-    } else {
-        return Promise.reject(new Error(response.statusText));
-    }
-}
-function parseJSON(response) {
-    return response.json();
-}
-//for opening link in new tab
-function openIndex() {
-    chrome.tabs.create({ active: true, url: recipes[rand].link });
-}
